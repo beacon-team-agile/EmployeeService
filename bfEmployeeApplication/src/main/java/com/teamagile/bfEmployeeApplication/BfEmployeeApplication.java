48310ddbf08c35d03a5282755145347c270cfc50
EigentlicherVogel <peregrinanatum@outlook.com>
2022-08-12 19:40:52 -0500

    initial application and dependency

---
+++ bfEmployeeApplication/src/main/java/com/teamagile/bfEmployeeApplication/BfEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.teamagile.bfEmployeeApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BfEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BfEmployeeApplication.class, args);
+	}
+
+}
